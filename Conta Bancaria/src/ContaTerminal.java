f8ef76404e60b43970ca96f174c59ab612be071d
Gustavo BS <gustavobarbosa40.gb@gmail.com>
2023-08-01 16:11:35 -0300

    Att

---
+++ Conta Bancaria/src/ContaTerminal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Por Favor, Digite seu nome:");
+
+        Scanner sc = new Scanner(System.in);
+        String nomeCliente = sc.nextLine();
+
+
+    }
+}
